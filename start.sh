0ffe944bf4d4c5b45d4a58c622c6deca49fa80fe
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-12-12 17:20:05 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-if [ -z $UPSTREAM_REPO ]
-then
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/FilmBee/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /VJ-FILTER-BOT 
 fi
+
 cd /VJ-FILTER-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
